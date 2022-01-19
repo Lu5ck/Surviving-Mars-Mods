239bf9d7a42d68b3c99899b80d314af936c6dca6
Lu5ck <6626980+Lu5ck@users.noreply.github.com>
2022-01-19 17:29:39 +0800

    Update items.lua

--- Global Birth Control Policies/items.lua
+++ Global Birth Control Policies/items.lua
@@ -4,18 +4,19 @@ PlaceObj('ModItemCode', {
 	'comment', "The one file to change them all",
 	'FileName', "Code/GlobalBirthControlPolicies.lua",
 }),
-PlaceObj('ModItemOptionToggle', {
-	'name', "GBCP_Toggle",
-	'comment', "Enable or Disable",
-	'DisplayName', "Double Click it",
-	'Help', "Default is Off",
-}),
 PlaceObj('ModItemOptionNumber', {
 	'name', "GBCP_MaxChildren",
 	'comment', "How many children?",
 	'DisplayName', "Maximum Children",
 	'Help', "Default is 20",
 	'DefaultValue', 20,
-	'MaxValue', 200,
+	'MaxValue', 500,
+}),
+PlaceObj('ModItemOptionToggle', {
+	'name', "GBCP_Toggle",
+	'comment', "Enable or Disable",
+	'DisplayName', "Double Click it",
+	'Help', "Default is Off",
+	'DefaultValue', true,
 }),
 }
