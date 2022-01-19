94356a94614083b2e356a6c6f548da97cb5a63d2
Lu5ck <6626980+Lu5ck@users.noreply.github.com>
2022-01-19 17:27:03 +0800

    Get CurrentModOptions properties directly
    I do not know how Surviving Mars load order works thus getting the properties directly will avoid outdated user settings.

--- Global Birth Control Policies/Code/GlobalBirthControlPolicies.lua
+++ Global Birth Control Policies/Code/GlobalBirthControlPolicies.lua
@@ -1,17 +1,3 @@
--- Get Mod Options
-local BirthControlToggle = CurrentModOptions.GBCP_Toggle
-local MaximumChildren = CurrentModOptions.GBCP_MaxChildren
-
--- Update user settings
-function OnMsg.ApplyModOptions(id)
-    if id ~= CurrentModId then
-        return
-    end
-
-    BirthControlToggle = CurrentModOptions.GBCP_Toggle
-    MaximumChildren = CurrentModOptions.GBCP_MaxChildren
-end
-
 -- Copy the original function
 local oldCalcBirth = Community.CalcBirth
 
@@ -19,13 +5,14 @@ local oldCalcBirth = Community.CalcBirth
 function Community:CalcBirth(...)
 
     -- Only proceed if mod is enabled else skip all these to original function
-    if BirthControlToggle == true then
+    if CurrentModOptions.GBCP_Toggle == true then
+        print(CurrentModOptions.GBCP_MaxChildren)
         -- Get overview data, in this case we need the children population
         local resource_overview = GetCityResourceOverview(UICity)
         local data = resource_overview.data
 
         -- If exceed set limit, end function
-        if data.children >= MaximumChildren then
+        if data.children >= CurrentModOptions.GBCP_MaxChildren then
             return
         end
     end
